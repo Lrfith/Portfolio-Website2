f0cd8139ef58cbc61371a6f5fcb200d282da4786
AS <98580462+Lrfith@users.noreply.github.com>
2025-10-26 16:43:42 +0700

    fix

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Navbar from './assets/components/Navbar'
 import Home from './assets/routes/Home'
-import { Route, Routes, HashRouter } from 'react-router-dom'
+import { Route, Routes, HashRouter, Router } from 'react-router-dom'
 import Skills from './assets/routes/Skills'
 import Projects from './assets/routes/Projects'
 import Contact from './assets/routes/Contact'
@@ -12,15 +12,13 @@ const App = () => {
     <>
       <div className='min-h-screen flex flex-col bg-gray-50'>
         <Navbar />
-        <HashRouter>
-        <Routes>
+        <Router>
           <Route path='/' element={<Home />} />
           <Route path='/skills' element={<Skills />} />
           <Route path='/projects' element={<Projects />} />
           <Route path='/certificates' element={<Certificates />} />
           <Route path='/contact' element={<Contact />} />
-        </Routes>
-        </HashRouter>
+        </Router>
       </div>
     </>
   )
